80f4b1de5b29896fcc3a7abf705114fd8a60f574
Ben Pemberton <benedictpemberton@gmail.com>
2022-01-26 19:43:26 +0000

    Have cards with book images that randomly sort themselves on button click.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import uniqid from "uniqid";
+import cardsInfo from "./components/cardsInfo";
+import Card from "./components/Card";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState(cardsInfo());
+
+  useEffect(() => {
+    console.log(cards);
+  });
+
+  function randomiseCards() {
+    return cards.sort(() => Math.random() - 0.5);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-wrap">
+      <div className="header">
+        <h1>The Memory Game</h1>
+      </div>
+      <div className="score-area">
+        <p>Score: {score}</p>
+        <button onClick={() => setScore(score + 1)}>click</button>
+        <p>Best score: {bestScore}</p>
+      </div>
+      <div className="card-grid">
+        {randomiseCards().map((card) => (
+          <Card key={uniqid()} {...card} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/cardsInfo.js
@@ -0,0 +1,77 @@
+import christmasPud from "./../images/christmas-pudding.jpg";
+import cloudCuckoo from "./../images/cloud-cuckoo-land.jpg";
+import homegoing from "./../images/homegoing.jpg";
+import lockeLomora from "./../images/lies-of-locke-lamora.jpg";
+import neverLetMeGo from "./../images/never-let-me-go.jpg";
+import manInHavana from "./../images/our-man-in-havana.jpg";
+import snuff from "./../images/snuff.jpg";
+import stoner from "./../images/stoner.jpg";
+import harryQuebert from "./../images/the-harry-quebert-affair.jpg";
+import nameOfTheWind from "./../images/the-name-of-the-wind.jpg";
+import shadowOfTheWind from "./../images/the-shadow-of-the-wind.jpg";
+import sterkarmHandshake from "./../images/the-sterkarm-handshake.jpg";
+
+export default function cardsInfo() {
+  return [
+    {
+      name: "Christmas Pudding",
+      image: christmasPud,
+      clicked: false,
+    },
+    {
+      name: "Cloud Cuckoo Land",
+      image: cloudCuckoo,
+      clicked: false,
+    },
+    {
+      name: "Homegoing",
+      image: homegoing,
+      clicked: false,
+    },
+    {
+      name: "Lies of Locke Lamora",
+      image: lockeLomora,
+      clicked: false,
+    },
+    {
+      name: "Never Let Me Go",
+      image: neverLetMeGo,
+      clicked: false,
+    },
+    {
+      name: "Our Man in Havana",
+      image: manInHavana,
+      clicked: false,
+    },
+    {
+      name: "Snuff",
+      image: snuff,
+      clicked: false,
+    },
+    {
+      name: "Stoner",
+      image: stoner,
+      clicked: false,
+    },
+    {
+      name: "The Harry Quebert Affair",
+      image: harryQuebert,
+      clicked: false,
+    },
+    {
+      name: "The Name of the Wind",
+      image: nameOfTheWind,
+      clicked: false,
+    },
+    {
+      name: "The Shadow of the Wind",
+      image: shadowOfTheWind,
+      clicked: false,
+    },
+    {
+      name: "The Sterkarm Handshake",
+      image: sterkarmHandshake,
+      clicked: false,
+    },
+  ];
+}
